e86617b99b2fccc01b7e792a77a2470e14a43b07
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-07 12:55:07 -0500

    add seed route to controller file

--- controllers/turtles.js
+++ controllers/turtles.js
@@ -7,5 +7,14 @@ const turtlesRouter = express.Router();
 // Import Turtle model
 const Turtle = require("../models/Turtle");
 
+// Import seed data
+const turtleSeed = require("../models/turtleSeed");
+
 // seed route
-turtlesRouter.get("/turtles/seed", (req, res) => {});
+turtlesRouter.get("/turtles/seed", (req, res) => {
+  Turtle.deleteMany({}, (err, deletedTurtles) => {
+    Turtle.create(turtleSeed, (err, allTurtles) => {
+      res.redirect("/turtles");
+    });
+  });
+});
